f619a127c263a4851c4f6947004ea0b4bac374f2
YeziTechie <yazdan.hka@gmail.com>
2026-07-22 18:13:29 +0330

    style: i don't remember

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns = [
@@ -11,5 +12,5 @@ urlpatterns = [
     path("api/payment/", include("payments.urls", namespace="payments")),
 ]
 
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
